688f3a70c6dac048345bcd15493305359799941f
Craig Loomis <cloomis@astro.princeton.edu>
2021-05-04 06:21:27 -0400

    Flesh out setup and go routines.

--- python/ics/pfilampsActor/Commands/LampsCmd.py
+++ python/ics/pfilampsActor/Commands/LampsCmd.py
@@ -1,3 +1,5 @@
+import time
+
 import opscore.protocols.keys as keys
 import opscore.protocols.types as types
 from opscore.utility.qstr import qstr
@@ -41,25 +43,69 @@ class LampsCmd(object):
 
         cmd_txt = cmd.cmd.keywords['raw'].values[0]
 
-        ret = self.pi.lampsCmd(cmd_txt, cmd=cmd)
+        ret = self.pi.lampsCmd(f'raw {cmd_txt}', cmd=cmd)
         cmd.finish('text=%s' % (qstr('returned: %s' % (ret))))
 
-    def prepare(self, cmd):
+    def setup(self, cmd):
         """Configure the calibration system lamps for the given exposure times. """
 
-        cmd.finish("text='Present and (probably) well'")
+        cmdkeys = cmd.cmd.keywords
+        if 'hgcd' in cmdkeys and 'halogen' in cmdkeys:
+            cmd.fail('text="halogen and hgcd cannot both be specified"')
+            return
+
+        lamps = []
+        for name in self.lampNames:
+            if name in cmdkeys:
+                val = cmdkeys[name].values[0]
+                val = int(val)
+                if name == 'halogen':
+                    name = 'cont'
+                lamps.append(f'{name}={val}')
+
+        if len(lamps) == 0:
+            cmd.fail('text="at least one lamp must be specified"')
+            return
+
+        setupCmd = f'setup {" ".join(lamps)}'
+        ret = self.pi.lampsCmd(setupCmd)
+        cmd.finish(f'text="{ret}"')
+
+    def _getStatus(self, cmd):
+        """Get current lamp status."""
+
+        ret = self.pi.lampsCmd('status')
+        ok, running, ready = ret.split()
+        if ok != 'OK':
+            raise RuntimeError(f'status is bad: {ret}')
+        running = bool(running)
+        ready = bool(ready)
+
+        return running, ready
 
     def go(self, cmd):
         """Given the already configured lamps, run the sequence """
 
-        cmd.finish()
+        while True:
+            running, ready = self._getStatus(cmd)
+            if not running:
+                cmd.fail('text="lamps are not configured"')
+                return
+            if ready:
+                break
+            time.sleep(0.2)
+
+        ret = self.pi.lampsCmd('go')
+        cmd.finish(f'text="{ret}"')
 
     def stop(self, cmd):
         """Given a running or merely configured sequence, stop it."""
 
-        cmd.finish()
+        ret = self.pi.lampsCmd('stop')
+        cmd.finish(f'text="{ret}"')
 
     def status(self, cmd):
         """Get current lamp status."""
 
-        cmd.finish()
+        ret = self.pi.lampsCmd('status')
+        cmd.finish(f'text="{ret}"')
